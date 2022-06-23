6ff79cf1f619f7a9f398d8b7ff44c0b2304c3e41
Juan Falco <juanfalco.21@gmail.com>
2022-06-23 16:23:51 -0300

    probando arreglos por 4ta vez

--- js/main.js
+++ js/main.js
@@ -113,35 +113,3 @@ function navHighlighter(){
     })
 }
 
-/****** CONTACTO-FORMULARIO ******/
-((d) => {
-    const $form = d.querySelector(".contacto_formulario");
-    // $loader = d.querySelector(".contact-form-loader"),
-    // $response = d.querySelector(".contactacto-formulario-responde");
-
-    $form.addEventListener("submit",(e) => {
-        e.preventDefault();
-        // $loader.classList.remove("none");
-        fetch("https://formsubmit.co/ajax/juan.falco21@gmail.com",{
-            method:"POST",
-            body:new FormData(e.target),
-        })
-            .then((res) => (res.ok ? res.json():Promise.reject(res)))
-            .then(json=>{
-                console.log(json);
-                location.hash = "#gracias";
-                $form.reset();
-            })
-            .catch((err) => {
-                console.log(err);
-                let message = err.statusText || "Ocurrio un error al enviar, intenta nuevamente";
-                // $response.querySelector("h3").innerHTML = `Eror ${err.status}:${message}`;
-            })
-            .finally(()=>{
-                // $loader.classList.add("none");
-                setTimeout(()=>{
-                    location.hash="#close"
-                },3000);
-            });     
-    });
-})(document);
